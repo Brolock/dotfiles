8c1fa2a9417d50ef4a638a9d86034c4f9fb5d05c
Brolock <marcelin.dupraz@gmail.com>
2025-08-06 21:00:18 +0200

    move to i3blocks

--- config/i3blocks/ip.py
+++ config/i3blocks/ip.py
@@ -6,4 +6,4 @@ import urllib.request
 with urllib.request.urlopen("https://ipv4.geojs.io/v1/ip/geo.json") as url:
     data = json.loads(url.read().decode())
     str = data['ip'] + " (" + data['city'] + ", " + data['country_code'] + ")"
-    print(str, end='')
+    print(str)
